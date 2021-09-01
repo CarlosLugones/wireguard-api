b61f718725e46d714e74054921d2f12ed6560158
Carlos Lugones <contact@lugodev.com>
2021-09-01 11:49:35 -0400

    feat: wireguard + fast api

---
+++ api.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+# We load the env vars from a .env file
+load_dotenv()
+
+# Token uses by master to send commands to this node
+TOKEN = os.getenv('LUGODEV_VPN_TOKEN')
+
+class Command(BaseModel):
+    token: str
+    command: str
+
+app = FastAPI()
+
+@app.post("/")
+async def run_command(command: Command):
+
+    if command.token == TOKEN:
+
+        try:
+
+            status = subprocess.check_output(
+                command.command,
+                shell=True,
+                stderr=subprocess.STDOUT
+            )
+
+        except Exception as e:
+
+            status = str(e)
+        
+        return {"status": status}
+    
+    return {"status": "forbidden"}
